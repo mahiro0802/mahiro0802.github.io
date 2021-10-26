f8025be2005cfeed527b0a0e1a5a1b41a1187aaa
mahiro <65147170+mahiro0802@users.noreply.github.com>
2021-10-26 17:04:42 +0900

    Delete multitwicasviewer directory

--- multitwicasviewer/js/index.js
+++
@@ -1,8 +0,0 @@
-function submittttt(){
-    var textarea = document.getElementById("textarea").value;
-    var ta = textarea.split("\n");
-    var ja = ta.join('","');
-    var json = "[\"" + ja + "\"]";
-
-    window.location.href = "viewer.html?user=" + json;
-}
